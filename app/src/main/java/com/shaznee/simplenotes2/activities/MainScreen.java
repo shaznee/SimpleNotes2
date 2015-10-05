c0d802bf1605dbb4e33eb07a401226da6481e6e6
Shaznee <mrmshaznee@hotmail.com>
2015-10-05 14:01:31 -0700

    Initial commit

---
+++ app/src/main/java/com/shaznee/simplenotes2/activities/MainScreen.java
@@ -0,0 +1,15 @@
+package com.shaznee.simplenotes2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shaznee.simplenotes2.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
